e73c6d8c3c819b9db93d54efcccc3b23913b42b1
Aliaga Kyazimov <kyazimov-2011@mail.ru>
2022-12-30 15:17:43 +0300

    Change file select.sql

--- select.sql
+++ select.sql
@@ -16,3 +16,51 @@ where name_nickname not like '% %';
 
 select name from song
 where name like '%Моя%' or name like '%My%';
+
+--select - запросы агрегирующие функции
+
+select genre_id, count(*) from genreartist 
+group by genre_id
+order by count(*) desc;
+
+select album_id, count(*) from song s 
+group by album_id 
+order by count(*) desc;
+
+select album_id, AVG(song_time) from song s 
+group by album_id 
+order by AVG(song_time) desc;
+
+select name_nickname from artist
+where id in (select artist_id from albumartist a 
+where album_id in (select id from album 
+where year_of_release < '2020-01-01' or year_of_release > '2020-12-31')
+group by artist_id
+order by artist_id);
+
+select name from collection
+where id in (select collection_id from collectionsong
+where song_id in (select id from song
+where album_id in (select album_id from albumartist
+where artist_id in (select id from artist
+where name_nickname like '%Скриптонит%'))));
+
+select title from album
+where id in (select album_id from albumartist
+where artist_id in (select artist_id from genreartist
+group by artist_id 
+having count(*) > 1));
+
+select name from song
+where id not in (select song_id from collectionsong);
+
+select name_nickname from artist
+where id in (select artist_id from albumartist
+where album_id in (select album_id from song
+where id in (select id from song
+where song_time in (select min(song_time) from song))));
+
+select title from album
+where id in (select album_id from song
+group by album_id
+having count(*) = 1);
